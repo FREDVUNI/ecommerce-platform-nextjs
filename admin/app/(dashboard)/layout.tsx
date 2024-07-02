36b8b309036fe89a9d5116a74e66d7f3660d5b04
Fred Vuni <fredvuni809@gmail.com>
2024-07-02 08:09:50 +0300

    feat: create the layout page

---
+++ admin/app/(dashboard)/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Borcella Admin Auth",
+  description: "Manage Borcella's store",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
