da5e2121aff3be117917a895eb07c9d0abc13109
Fred Vuni <fredvuni809@gmail.com>
2024-07-04 00:33:32 +0300

    chore: center sign in and up

--- admin/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ admin/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,8 +2,10 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="h-screen flex justify-center">
+    <div className="h-screen flex justify-center items-center">
       <SignUp />;
     </div>
   );
 }
+
+After clicking "Donate Now," an error page appears, and the Donor Comments and card number details are cleared., In testing environments, live payment gateways like authorize.net are often not connected or fully configured, leading to errors during the transaction process.
