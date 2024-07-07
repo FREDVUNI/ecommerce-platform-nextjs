d54a8d079721a822934a16a1284e96f250215572
Fred Vuni <fredvuni809@gmail.com>
2024-07-07 10:44:11 +0300

    chore: adjust the root layout

--- admin/app/(dashboard)/layout.tsx
+++ admin/app/(dashboard)/layout.tsx
@@ -24,10 +24,12 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={poppins.className}>
-          <div className="flex max-lg:flex-col">
+          <div className="flex h-screen">
             <LeftSideBar />
-            <TopBar />
-            <div className="flex-1">{children}</div>
+            <div className="flex flex-col flex-1">
+              <TopBar />
+              <div className="flex-1 overflow-y-auto p-4">{children}</div>
+            </div>
           </div>
         </body>
       </html>
