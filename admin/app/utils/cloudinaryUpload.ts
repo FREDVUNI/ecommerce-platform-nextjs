85415d5d3f0ee34377f72d90675d57a99b57b829
Fred Vuni <fredvuni809@gmail.com>
2024-07-21 14:13:13 +0300

    feat: create collection form with image upload

---
+++ admin/app/utils/cloudinaryUpload.ts
@@ -0,0 +1,21 @@
+import cloudinary from "cloudinary";
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadImage = async (directory: string, imagePath: string) => {
+  try {
+    const folderPath = `borcelle/${directory}`;
+    const result = await cloudinary.v2.uploader.upload(imagePath, {
+      folder: folderPath,
+    });
+
+    return { success: true, result };
+  } catch (error) {
+    console.error("Error uploading image to Cloudinary:", error);
+    return { success: false, error };
+  }
+};
