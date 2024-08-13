a331a759641ef9446a003983d0d0a04806fdb617
Fred Vuni <fredvuni809@gmail.com>
2024-08-13 17:34:54 +0300

    feat: create the collection schema

---
+++ admin/app/lib/models/Collection.tsx
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const collectionSchema = new mongoose.Schema({
+  title: {
+    type: "string",
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: "string",
+    required: true,
+  },
+  image: {
+    type: "string",
+    required: true,
+  },
+  products: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "products",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const collection =
+  mongoose.models.Collection || mongoose.model("Collection", collectionSchema);
+
+export default collection;
