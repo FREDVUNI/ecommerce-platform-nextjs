01fb3f54346fea195994e48e014ac778183bf64b
Fred Vuni <fredvuni809@gmail.com>
2024-09-28 16:19:46 +0300

    chore: comment out logs

--- admin/app/lib/database/db.tsx
+++ admin/app/lib/database/db.tsx
@@ -3,23 +3,24 @@ import mongoose from "mongoose";
 let isConnected: boolean = false;
 
 const connectDB = async (): Promise<void> => {
-  if (isConnected) return console.log("Already connected.");
+  if (isConnected) return;
+  // console.log("Already connected.");
 
   const uri = process.env.MONGO_URI;
-  if (!uri) {
-    console.log("MONGO_URI is not defined in the environment variables.");
+  if (!uri)
+    // console.log("MONGO_URI is not defined in the environment variables.");
     return;
-  }
 
   try {
     await mongoose.connect(uri, {
       dbName: process.env.MONGO_DB_NAME,
     });
     isConnected = true;
-    console.log("Database connected.");
+    // console.log("Database connected.");
   } catch (error) {
-    console.log("Error connecting to the database:", error);
+    // console.log("Error connecting to the database:", error);
     isConnected = false;
+    return;
   }
 };
 
