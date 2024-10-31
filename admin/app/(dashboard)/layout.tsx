9ed1134a4fd3334168393013d3766467d61a82ba
Fred Vuni <fredvuni809@gmail.com>
2024-10-31 13:53:11 +0300

    chore: add the toast provider for toast messages

--- admin/app/(dashboard)/layout.tsx
+++ admin/app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import ToastProvider from "../lib/ToastProvider";
 import TopBar from "../components/layout/TopBar";
 import LeftSideBar from "../components/layout/LeftSideBar";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={poppins.className}>
+          <ToastProvider />
           <div className="flex h-screen">
             <LeftSideBar />
             <div className="flex flex-col flex-1">
