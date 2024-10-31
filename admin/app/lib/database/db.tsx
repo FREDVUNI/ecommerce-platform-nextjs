6fce04b0c05de4a79cef9ba05dbf3a51c7e52b1b
Fred Vuni <fredvuni809@gmail.com>
2024-10-31 13:52:43 +0300

    chore: update the db connection

--- admin/app/lib/database/db.tsx
+++ admin/app/lib/database/db.tsx
@@ -4,21 +4,16 @@ let isConnected: boolean = false;
 
 const connectDB = async (): Promise<void> => {
   if (isConnected) return;
-  // console.log("Already connected.");
 
   const uri = process.env.MONGO_URI;
-  if (!uri)
-    // console.log("MONGO_URI is not defined in the environment variables.");
-    return;
+  if (!uri) return;
 
   try {
     await mongoose.connect(uri, {
       dbName: process.env.MONGO_DB_NAME,
     });
     isConnected = true;
-    // console.log("Database connected.");
   } catch (error) {
-    // console.log("Error connecting to the database:", error);
     isConnected = false;
     return;
   }
