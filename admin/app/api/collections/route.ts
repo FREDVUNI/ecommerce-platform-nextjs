726d4f18893daef6ae2a3f1bb5afbbd84858c82b
Fred Vuni <fredvuni809@gmail.com>
2024-11-04 07:45:51 +0300

    chore: import database

--- admin/app/api/collections/route.ts
+++ admin/app/api/collections/route.ts
@@ -1,4 +1,3 @@
-import connectDB from "@/app/lib/database/db";
 import Collection from "@/app/lib/models/Collection";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse, NextRequest } from "next/server";
