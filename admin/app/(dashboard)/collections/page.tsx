0c00b66c4172e55d79506fb09fae58427c2adcfa
Fred Vuni <fredvuni809@gmail.com>
2024-11-23 11:30:49 +0300

    chore: update the collection page

--- admin/app/(dashboard)/collections/page.tsx
+++ admin/app/(dashboard)/collections/page.tsx
@@ -3,7 +3,13 @@ import React from "react";
 type Props = {};
 
 const Collection = (props: Props) => {
-  return <div>Collection</div>;
+  return (
+    <div className="p-10 bg-white shadow-sm">
+      <h2 className="text-2xl font-medium mb-4 text-gray-500">
+        Collections
+      </h2>
+    </div>
+  );
 };
 
 export default Collection;
