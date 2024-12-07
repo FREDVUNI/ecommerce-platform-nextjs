228cc005e4fa80c5bf1e373da731e19ab140b137
Fred Vuni <fredvuni809@gmail.com>
2024-12-07 14:01:29 +0300

    chore: connect the db to create collection

--- admin/app/api/collections/route.ts
+++ admin/app/api/collections/route.ts
@@ -1,13 +1,16 @@
+import connectDB from "@/app/lib/database/db";
 import Collection from "@/app/lib/models/Collection";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse, NextRequest } from "next/server";
 
 export const POST = async (req: NextRequest) => {
   try {
+    await connectDB();
+
     const { userId } = auth();
     if (!userId) return new NextResponse("unathorized", { status: 401 });
 
-    const { title, description, image } = await req.json();
+    const { title, description, images } = await req.json();
 
     const existingCollection = await Collection.findOne({
       title,
@@ -18,7 +21,13 @@ export const POST = async (req: NextRequest) => {
         status: 400,
       });
 
-    if (!title || !description || !image)
+    if (
+      !title ||
+      !description ||
+      !images ||
+      !Array.isArray(images) ||
+      images.length === 0
+    )
       return new NextResponse("All fields are required.", {
         status: 400,
       });
@@ -26,16 +35,20 @@ export const POST = async (req: NextRequest) => {
     const newCollection = await Collection.create({
       title,
       description,
-      image,
+      image: images[0],
       userId,
     });
 
     await newCollection.save();
 
-    return new NextResponse(newCollection.json(), {
+    return new NextResponse(JSON.stringify(newCollection.toObject()), {
       status: 201,
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
   } catch (error) {
-    return new NextResponse("Internal server error", { status: 500 });
+    console.log(error);
+    return new NextResponse("Internal server error " + error, { status: 500 });
   }
 };
