3af3afe8bffb9c3604ac2b851ff631c1ff3e773a
Fred Vuni <fredvuni809@gmail.com>
2025-01-05 12:28:28 +0300

    feat: create table using DataTable

--- admin/app/(dashboard)/collections/page.tsx
+++ admin/app/(dashboard)/collections/page.tsx
@@ -1,11 +1,54 @@
 "use client";
 
-import { columns, DataTable } from "@/app/components/custom ui/DataTable";
+import { DataTable } from "@/app/components/custom ui/DataTable";
 import React, { useEffect } from "react";
+import { ColumnDef } from "@tanstack/react-table";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 
 type Props = {};
+const columns: ColumnDef<any, any>[] = [
+  {
+    header: "Title",
+    accessorKey: "title",
+  },
+  {
+    header: "Description",
+    accessorKey: "description",
+  },
+  {
+    header: "Image",
+    accessorKey: "image",
+    cell: ({ row }) => (
+      <img
+        src={row.original.image}
+        alt={row.original.title}
+        className="w-16 h-16 object-cover rounded-md"
+      />
+    ),
+  },
+  {
+    header: "Actions",
+    cell: ({ row }) => (
+      <div className="flex space-x-2">
+        <Button
+          variant="outline"
+          size="sm"
+          // onClick={() => handleEdit(row.original)}
+        >
+          {/* <Edit className="w-4 h-4" /> */}
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          // onClick={() => handleDelete(row.original.id)}
+        >
+          {/* <Trash className="w-4 h-4 text-red-500" /> */}
+        </Button>
+      </div>
+    ),
+  },
+];
 
 const Collections = (props: Props) => {
   const [loading, setLoading] = React.useState(true);
@@ -32,12 +75,17 @@ const Collections = (props: Props) => {
     <div className="px-1">
       <div className="flex items-center justify-between">
         <h2 className="text-2xl font-medium mb-4 text-gray-500">Collections</h2>
-        <Button className="bg-blue-1 text-white">
+        <Button className="bg-blue-1 text-white mb-4">
           <Plus className="w-4 h-4 mr-2" />
           create collection
         </Button>
       </div>
-      <DataTable columns={columns} data={collections} />
+      <DataTable
+        columns={columns}
+        data={collections}
+        searchKey="title"
+        loading={loading}
+      />{" "}
     </div>
   );
 };
