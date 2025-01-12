24d5410889d40743055d1c39bf78312cb30236ba
Fred Vuni <fredvuni809@gmail.com>
2025-01-12 12:20:26 +0300

    chore: delete functionality

---
+++ admin/app/api/collections/[collectionId]/route.ts
@@ -0,0 +1,25 @@
+import connectDB from "@/app/lib/database/db";
+import { auth } from "@clerk/nextjs/server";
+import Collection from "@/app/lib/models/Collection";
+import { NextRequest, NextResponse } from "next/server";
+
+export const Delete = async (
+  req: NextRequest,
+  { params }: { params: { collectionId: string } }
+) => {
+  try {
+    const userId = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    await connectDB();
+
+    await Collection.findByIdAndDelete(params.collectionId);
+
+    return new NextResponse("collection has been deleted.", { status: 204 });
+  } catch (err) {
+    console.log("[collectionId_DELETE]", err);
+  }
+};
