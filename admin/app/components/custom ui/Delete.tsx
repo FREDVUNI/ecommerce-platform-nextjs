b481b54c7689c4895361e5915efc816a1ec6e0d3
Fred Vuni <fredvuni809@gmail.com>
2025-01-12 12:20:36 +0300

    feat: add delete dialog

--- admin/app/components/custom ui/Delete.tsx
+++ admin/app/components/custom ui/Delete.tsx
@@ -1,15 +1,66 @@
-import { Trash } from "lucide-react";
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
 import { Button } from "@/components/ui/button";
-import React from "react";
+import { Trash } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+interface DeleteProps {
+  id: string;
+}
 
-type Props = {};
+export const Delete: React.FC<DeleteProps> = ({ id }) => {
+  const [loading, setLoading] = useState(false);
 
-const Delete = (props: Props) => {
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch(`/api/collections/${id}`, {
+        method: "DELETE",
+      });
+
+      if (res.ok) {
+        setLoading(false);
+        window.location.href = "/collections";
+        toast.success("Collection has been deleted.");
+      }
+    } catch (err) {
+      console.log("[collectionId_DELETE]", err);
+    }
+  };
   return (
-    <Button className="bg-red-1 text-white">
-      <Trash className="h-4 w-4" />
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline" size="sm">
+          <Trash className="w-4 h-4 text-red-500" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent className="bg-white text-gray-800">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete this
+            collection.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction className="bg-red-700 text-white" onClick={onDelete}>
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
-
-export default Delete;
