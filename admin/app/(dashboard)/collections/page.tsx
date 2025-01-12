f6e6adb4e7f3d6fb9b4dd55ba52d75f784f1aae2
Fred Vuni <fredvuni809@gmail.com>
2025-01-12 12:19:59 +0300

    chore: move collection items to collection columns component

--- admin/app/(dashboard)/collections/page.tsx
+++ admin/app/(dashboard)/collections/page.tsx
@@ -6,60 +6,10 @@ import { ColumnDef } from "@tanstack/react-table";
 import { Button } from "@/components/ui/button";
 import { Edit, Plus, Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { Delete } from "@/app/components/custom ui/Delete";
+import { columns } from "./CollectionColumns";
 
 type Props = {};
-const columns: ColumnDef<any, any>[] = [
-  {
-    header: "Image",
-    accessorKey: "image",
-    cell: ({ row }) => (
-      <img
-        src={row.original.image}
-        alt={row.original.title}
-        className="w-16 h-16 object-cover rounded-md"
-      />
-    ),
-  },
-  {
-    header: "Title",
-    accessorKey: "title",
-    cell: ({ row }) => (
-      <div className="truncate max-w-xs" title={row.original.title}>
-        {row.original.title}
-      </div>
-    ),
-  },
-  {
-    header: "Products",
-    accessorKey: "product",
-    cell: ({ row }) => (
-      <div className="max-w-xs" title={row.original.products}>
-        {row.original.products.length}
-      </div>
-    ),
-  },
-  {
-    header: "Actions",
-    cell: ({ row }) => (
-      <div className="flex space-x-2">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => handleEdit(row.original)}
-        >
-          <Edit className="w-4 h-4" />
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => handleDelete(row.original.id)}
-        >
-          <Trash className="w-4 h-4 text-red-500" />
-        </Button>
-      </div>
-    ),
-  },
-];
 
 const Collections = (props: Props) => {
   const router = useRouter();
@@ -106,10 +56,3 @@ const Collections = (props: Props) => {
 };
 
 export default Collections;
-function handleEdit(original: any): void {
-  throw new Error("Function not implemented.");
-}
-
-function handleDelete(id: any): void {
-  throw new Error("Function not implemented.");
-}
