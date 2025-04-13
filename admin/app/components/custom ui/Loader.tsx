c374b8ecabb3adb42034841cdc400f7de60fece4
Fred Vuni <fredvuni809@gmail.com>
2025-04-13 13:26:04 +0300

    fix: change Loader component to synchronous function

--- admin/app/components/custom ui/Loader.tsx
+++ admin/app/components/custom ui/Loader.tsx
@@ -1,4 +1,4 @@
-const Loader = async () => {
+const Loader = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="animate-spin rounded-full border-t-4 border-blue-400 border-solid h-12 w-12"></div>
